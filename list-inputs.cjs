9bcbf0f0ff5d521bd4bafd452ccca800b792333b
Flora <2758649435@qq.com>
2026-05-17 23:40:54 +0800

    chore: 清理临时脚本文件

--- list-inputs.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const tsContent = fs.readFileSync('src/data/aiConversationLog.ts', 'utf-8');
-const arrayMatch = tsContent.match(/export const aiConversationLogEntries: AiConversationLogEntry\[\] = (\[[\s\S]*\]);/);
-const entries = new Function('return ' + arrayMatch[1])();
-entries.forEach((e, i) => console.log(i + ': ' + e.userInput.slice(0, 50).replace(/\n/g, ' ')));
